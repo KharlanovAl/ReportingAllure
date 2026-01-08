82cc13d0ec8af6fc623a367614bfbbd600f23435
Aleksey Kharlanov <harlan14@mail.ru>
2026-01-08 23:25:55 +0700

    new photo and test working all

--- src/test/java/ru/netology/testirovanie/test/AuthentificationTest.java
+++ src/test/java/ru/netology/testirovanie/test/AuthentificationTest.java
@@ -55,7 +55,7 @@ AuthentificationTest {
 
         $("[data-test-id=login] input").setValue(unregisteredUser.getLogin());
         $("[data-test-id=password] input").setValue(unregisteredUser.getPassword());
-        $("[data-test-id=action-login]");
+        $("[data-test-id=action-login]").click();
 
         $("[data-test-id=error-notification] .notification__content")
                 .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
